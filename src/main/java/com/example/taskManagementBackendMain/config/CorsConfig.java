c80e6ceacdb14f43164b43f41bb2000b568a9ed0
Meetakshi08 <meetakshijaiswal@gmail.com>
2026-05-04 18:25:03 +0530

    Fixed CORS

--- src/main/java/com/example/taskManagementBackendMain/config/CorsConfig.java
+++ src/main/java/com/example/taskManagementBackendMain/config/CorsConfig.java
@@ -3,8 +3,8 @@ package com.example.taskManagementBackendMain.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -16,18 +16,37 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
+        // Frontend URLs
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://task-manager-frontend.onrender.com"
+                )
         );
 
+        // Allowed Methods
         configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "PATCH")
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "PATCH",
+                        "OPTIONS"
+                )
         );
 
+        // Allowed Headers
         configuration.setAllowedHeaders(
                 List.of("*")
         );
 
+        // Exposed Headers
+        configuration.setExposedHeaders(
+                List.of("Authorization")
+        );
+
+        // Allow Credentials
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
--- src/main/java/com/example/taskManagementBackendMain/service/security/SecurityConfig.java
+++ src/main/java/com/example/taskManagementBackendMain/service/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/auth/login",
                                 "/auth/signUp",
                                 "/auth/register"
