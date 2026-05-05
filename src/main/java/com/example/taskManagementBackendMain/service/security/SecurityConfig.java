56ad83eb2ef8a353860ca975cfa27241cce1cfda
Meetakshi08 <meetakshijaiswal@gmail.com>
2026-05-05 17:45:34 +0530

    Updated final SecurityConfig

--- src/main/java/com/example/taskManagementBackendMain/service/security/SecurityConfig.java
+++ src/main/java/com/example/taskManagementBackendMain/service/security/SecurityConfig.java
@@ -6,25 +6,37 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    // BCrypt Password Encoder Bean
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Spring Security Configuration
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                // Disable CSRF
                 .csrf(csrf -> csrf.disable())
 
+                // Enable CORS
                 .cors(Customizer.withDefaults())
 
+                // Public Routes
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/login",
                                 "/auth/signUp"
                         ).permitAll()
 
+                        // Secure all other routes
                         .anyRequest().authenticated()
                 );
 
