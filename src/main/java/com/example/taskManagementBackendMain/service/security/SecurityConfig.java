85aaa206f416af846521149a7b37c6a425e0aa31
Meetakshi08 <meetakshijaiswal@gmail.com>
2026-05-05 17:37:18 +0530

    Fixed SecurityConfig build issue

--- src/main/java/com/example/taskManagementBackendMain/service/security/SecurityConfig.java
+++ src/main/java/com/example/taskManagementBackendMain/service/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.taskManagementBackendMain.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,13 +15,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                // Disable CSRF
                 .csrf(csrf -> csrf.disable())
 
-                // Enable CORS
                 .cors(Customizer.withDefaults())
 
-                // Authorize Requests
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/login",
